8331cd6a8ad12e5f512d04ed3bd64435c884e39b
Simon Thamer <thamers@edu.htl-villach.at>
2022-05-15 16:31:20 +0200

    removed old shell files

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /opt/git/simpsonbot
-/usr/bin/screen -dm -S bot -m "/opt/git/simpsonbot/start_bot.sh"
-
--- start_bot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /opt/git/simpsonbot
-/usr/bin/python3 /opt/git/simpsonbot/main.py
