52589294281350a76f405eb36c02b6161f3cabb3
Simon Thamer <thamers@edu.htl-villach.at>
2022-05-15 16:46:19 +0200

    now starting from main.py instead of bot.py

--- src/bot.py
+++ src/bot.py
@@ -2,15 +2,16 @@ import discord
 import os # default module
 from decouple import config
 
-extensions = [
-    "clear",
-    "duck"
-]
+def start_bot():
+    extensions = [
+        "clear",
+        "duck"
+    ]
 
-bot = discord.Bot(debug_guilds=[170834267721564160], intents=discord.Intents.all(),
-                  activity=discord.Activity(type=discord.ActivityType.playing, name="/help - by LordSimpson"))
+    bot = discord.Bot(debug_guilds=[170834267721564160], intents=discord.Intents.all(),
+                      activity=discord.Activity(type=discord.ActivityType.playing, name="/help - by LordSimpson"))
 
-for cog in extensions:
-    bot.load_extension(f"cogs.{cog}")
+    for cog in extensions:
+        bot.load_extension(f"src.cogs.{cog}")
 
-bot.run(config('TOKEN')) # run the bot with the token
+    bot.run(config('TOKEN')) # run the bot with the token
